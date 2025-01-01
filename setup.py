ebbc9da8e84a418b493a6ec5af072387456c7af3
CEOXeon <127042438+CEOXeon@users.noreply.github.com>
2025-01-01 19:04:53 +0100

    Restructured setup.py

--- setup.py
+++ setup.py
@@ -3,31 +3,35 @@ from pathlib import Path
 import setuptools
 
 VERSION = "0.0.1"  # PEP-440
-
 NAME = "translatium"
-
+DESCRIPTION = "A pure Python i18n library for your Python projects."
+URL="https://github.com/CEOXeon/Translatium"
+PROJECT_URLS = {
+    "Source Code": "https://github.com/CEOXeon/Translatium",
+}
+AUTHOR = "Louis Zimmermann (@CEOXeon)"
+AUTHOR_EMAIL = "louis-github@tutanota.com"
+LICENSE = "Apache-2.0"
+CLASSIFIERS = [
+    "Development Status :: 1 - Planning",
+    "Intended Audience :: Developers",
+    "Programming Language :: Python :: 3",
+    "Programming Language :: Python :: 3.12",
+]
 INSTALL_REQUIRES = [
     "pyyaml",
 ]
 
-
 setuptools.setup(
     name=NAME,
     version=VERSION,
-    description="A pure Python i18n library for your Python projects.",
-    url="https://github.com/CEOXeon/Translatium",
-    project_urls={
-        "Source Code": "https://github.com/CEOXeon/Translatium",
-    },
-    author="Louis Zimmermann (@CEOXeon)",
-    author_email="louis-github@tutanota.com",
-    license="Apache-2.0",
-    classifiers=[
-        "Development Status :: 1 - Planning",
-        "Intended Audience :: Developers",
-        "Programming Language :: Python :: 3",
-        "Programming Language :: Python :: 3.12",
-    ],
+    description=DESCRIPTION,
+    url=URL,
+    project_urls=PROJECT_URLS,
+    author=AUTHOR,
+    author_email=AUTHOR_EMAIL,
+    license=LICENSE,
+    classifiers=CLASSIFIERS,
     #python_requires=">=3.8",
     # Requirements
     install_requires=INSTALL_REQUIRES,
