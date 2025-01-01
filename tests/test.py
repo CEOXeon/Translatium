06fd624bf39bae294ac84024ed0ee720b5dbfa61
CEOXeon <127042438+CEOXeon@users.noreply.github.com>
2025-01-01 19:40:10 +0100

    Updated Tests to work on the fly

--- tests/test.py
+++ tests/test.py
@@ -1,6 +1,14 @@
+import sys
+import os
+
+# Add the src directory to the sys.path
+sys.path.insert(0, os.path.abspath(os.path.join(os.path.dirname(__file__), '../src')))
+
 from translatium import translatium
 
-translatium.init_translatium('locales/', 'en_US')
+# Use an absolute path for the locales directory
+locales_path = os.path.abspath(os.path.join(os.path.dirname(__file__), 'locales'))
+translatium.init_translatium(locales_path, 'en_US')
 translatium.set_language('de_DE')
 
 print(translatium.translation('hello_message'))
