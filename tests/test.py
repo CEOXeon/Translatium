d6ca6eb5df2161e2548cf46de56dd28b8122eaa5
CEOXeon <127042438+CEOXeon@users.noreply.github.com>
2025-01-03 19:31:03 +0100

    Updated Tests
    We are now using Pytest

--- tests/test.py
+++ tests/test.py
@@ -1,14 +1,14 @@
 import sys
 import os
+import pytest
 
-# Add the src directory to the sys.path
+
 sys.path.insert(0, os.path.abspath(os.path.join(os.path.dirname(__file__), '../src')))
 
 from translatium import translatium
 
-# Use an absolute path for the locales directory
-locales_path = os.path.abspath(os.path.join(os.path.dirname(__file__), 'locales'))
-
+# Get absolute path to locales directory
+LOCALES_PATH = os.path.abspath(os.path.join(os.path.dirname(__file__), 'locales'))
 
 
 
@@ -16,7 +16,32 @@ locales_path = os.path.abspath(os.path.join(os.path.dirname(__file__), 'locales'
 #                      # ACTUAL TESTS                      #
 ############################################################
 
-translatium.init_translatium(locales_path, 'en_US')
-translatium.set_language('de_DE')
+@pytest.mark.dependency()
+def test_import():
+    """Test that the module can be imported"""
+    assert translatium is not None
 
-print(translatium.translation('hello_message'))
+@pytest.mark.dependency(depends=["test_import"])
+def test_init():
+    """Test initializing with locales path and fallback language"""
+    translatium.init_translatium(LOCALES_PATH, 'en_US')
+    
+@pytest.mark.dependency(depends=["test_init"])
+def test_set_language():
+    """Test setting the active language"""
+    translatium.set_language('de_DE')
+    assert translatium._language == 'de_DE'
+
+@pytest.mark.dependency(depends=["test_set_language"])
+def test_translations():
+    """Test that translations work correctly"""
+    # Initialize first
+    translatium.init_translatium(LOCALES_PATH, 'en_US')
+    translatium.set_language('de_DE')
+    
+    # Test German translation
+    assert translatium.translation('hello_message') == 'Hallo Welt!'
+    
+    # Test fallback to English
+    translatium.set_language('invalid')
+    assert translatium.translation('hello_message') == 'Hello World! %{name}'
