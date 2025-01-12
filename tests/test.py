e7d681a30bf28d991d961f04279232d348a63aa0
CEOXeon <127042438+CEOXeon@users.noreply.github.com>
2025-01-12 18:21:53 +0100

    Updated Easter Eggs

--- tests/test.py
+++ tests/test.py
@@ -82,7 +82,7 @@ def test_more_depth_in_translations(monkeypatch):
 ############################################################
 
 EASTER_EGGS_PATH = Path(__file__).resolve().parent.parent / "easter_eggs.hidden"
-if not EASTER_EGGS_PATH.exists():
+if EASTER_EGGS_PATH.exists():
     def test_stupid_easter_eggs():
         import antigravity
         antigravity.fly()
