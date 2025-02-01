bf65c24bbd987560e6498b8c400cbf4ffae6d343
CEOXeon <127042438+CEOXeon@users.noreply.github.com>
2025-02-01 19:59:17 +0100

    Added Better Pluralization Support

--- src/translatium/i18n/strings.py
+++ src/translatium/i18n/strings.py
@@ -30,3 +30,38 @@ def translation(translation_key: str, **kwargs) -> str:
     else:
         raise TranslationError(
             f"Translation key '{translation_key}' not found in selected language '{get_config()["language"]}' or fallback language '{get_config()["fallback_language"]}'", translation_key)
+
+def pluralize(translation_key: str, count: int) -> str:
+    '''
+    Gets the translation for a specific key in the selected language and pluralizes it.
+    If the translation is not found in the selected language, the fallback language is used.
+
+    Parameters:
+    - translation_key: The key for the translation
+    - count: The count to determine the plural form
+
+    Returns: A string with the translation
+    '''
+    # Helper function to get translation from a specific language
+    def get_translation(language, keys):
+        translation = get_translations().get(language, {})
+        for key in keys:
+            if isinstance(translation, dict):
+                translation = translation.get(key)
+            else:
+                return None
+        return translation
+    keys = translation_key.split('.')
+    translation = get_translation(get_config()["language"], keys) or get_translation(get_config()["fallback_language"], keys)
+    if translation:
+        if count == 1:
+            return translation.get('one')
+        elif count > 1:
+            return translation.get('many')
+        elif count == 0:
+            return translation.get('none')
+        else:
+            return translation.get('default')
+    else:
+        raise TranslationError(
+            f"Translation key '{translation_key}' not found in selected language '{get_config()["language"]}' or fallback language '{get_config()["fallback_language"]}'", translation_key)
--- tests/test.py
+++ tests/test.py
@@ -76,6 +76,42 @@ def test_more_depth_in_translations(monkeypatch):
     assert translatium.translation('mail.one') == 'You have one new mail'
     assert translatium.translation('mail.many') == 'You have many new mails'
 
+@pytest.mark.dependency(depends=["test_more_depth_in_translations"])
+def test_pluralize(monkeypatch):
+    translatium.init_translatium(LOCALES_PATH, 'en_US')
+    translatium.set_config('language', 'de_DE')
+    translatium.set_config('fallback_language', 'de_DE')
+    data= {
+        "en_US": {
+            "mail": {
+                "default": "You have new mails",
+                "one": "You have one new mail",
+                "many": "You have many new mails",
+                "none": "You have no new mails"
+            }
+        },
+        "de_DE": {
+            "mail": {
+                "default": "Sie haben neue E-Mails",
+                "one": "Sie haben eine neue E-Mail",
+                "many": "Sie haben viele neue E-Mails",
+                "none": "Sie haben keine E-Mails"
+            }
+        }
+    }
+    change_translations_module_scope(monkeypatch, data)
+    assert translatium.pluralize('mail', 1) == 'Sie haben eine neue E-Mail'
+    assert translatium.pluralize('mail', 2) == 'Sie haben viele neue E-Mails'
+    assert translatium.pluralize('mail', 3) == 'Sie haben viele neue E-Mails'
+    assert translatium.pluralize('mail', 0) == 'Sie haben keine E-Mails'
+    assert translatium.pluralize('mail', -1) == "Sie haben neue E-Mails"
+    translatium.set_config("language", 'en_US')
+    assert translatium.pluralize('mail', 1) == 'You have one new mail'
+    assert translatium.pluralize('mail', 2) == 'You have many new mails'
+    assert translatium.pluralize('mail', 3) == 'You have many new mails'
+    assert translatium.pluralize('mail', 0) == 'You have no new mails'
+    assert translatium.pluralize('mail', -1) == "You have new mails"
+
 
 
 ############################################################
