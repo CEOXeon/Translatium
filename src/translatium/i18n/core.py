26beb314fc054aa5dd343fee4b33f5dc55a26cbe
CEOXeon <127042438+CEOXeon@users.noreply.github.com>
2025-07-19 20:26:01 +0200

    Code Standard changed to OOP

--- src/translatium/i18n/core.py
+++ src/translatium/i18n/core.py
@@ -1,71 +1,125 @@
 from pathlib import Path
 
 from typeguard import typechecked
+import yaml
+import json
 
-from .config import get_config, get_translations, set_config, set_translations
 from ..helpers.errors import TranslationError
-from ..helpers.utils import deprecated
-from ..helpers.file_io import load_translations
-
+from ..helpers.utils import TranslationsType
 
 @typechecked
-def init_translatium(path: Path, fallback: str) -> None:
-    '''
-    Initialize translatium. It does automatically load the translations and checks for errors.
+class Translator():
+    def __init__(self, path: Path = Path(), language: str = "", fallback_language: str = "") -> None:
+        self.path: Path = path
+        self.language: str = language
+        self.fallback_language: str = fallback_language
+        self.translations: TranslationsType = {}
+        self.load_translations()
+        return None
 
-    Parameters:
-    - path: Path to the directory containing the translation files
-    - fallback: The fallback language to use when a translation is not found in the selected language
+    def __repr__(self):
+        return f"Translator(path={self.path}, language={self.language}, fallback_language={self.fallback_language}, translations={self.translations})"
 
-    Returns: None
-    '''
-    set_translations(load_translations(path))
-    set_config("fallback_language", fallback)
-    checks()
-    return None
+    def __str__(self):
+        return self.__repr__()
 
-@typechecked
-def checks() -> None:
-    '''
-    Check if the translations are valid and if the fallback language is available.
+############################################################
+#                   # GETTERS AND SETTERS                  #
+############################################################
+    def set_language(self, language: str) -> None:
+        if language not in self.translations:
+            raise TranslationError("Language not found", language)
+        self.language = language
+        return None
 
-    Returns: None
-    '''
-    # Check if fallback language is available
-    def check_fallback_language():
-        if get_config()["fallback_language"] not in get_translations():
-            raise TranslationError("Fallback Language not found", get_config()["fallback_language"])
-    # Check if all keys in the languages (except the fallback language) are present in the fallback language
-    def check_translation_keys():
-        fallback_keys = set(get_translations()[get_config()["fallback_language"]].keys())
-        for lang, translations in get_translations().items():
-            if lang == get_config()["fallback_language"]:
-                continue
-            missing_keys = set(translations.keys()) - fallback_keys
-            if missing_keys:
-                raise TranslationError(f"Translation keys {missing_keys} in language '{lang}' not found in fallback language", missing_keys)
-    check_fallback_language()
-    check_translation_keys()
-    return None
+    def set_fallback_language(self, fallback_language: str) -> None:
+        if fallback_language not in self.translations and fallback_language != self.language:
+            raise TranslationError("Fallback Language not found", fallback_language)
+        self.fallback_language = fallback_language
+        return None
 
-@deprecated("Use set_config instead", "v0.3.0")
-@typechecked
-def set_language(language: str) -> None:
-    '''
-    Sets the preferred language for translations.
-    If the language is not available, an error is raised.
-    If the language is set to "invalid", the laguage is changed to the string invalid. This doen for testing purposes.
+    def set_path(self, path: Path) -> None:
+        if not path.exists():
+            raise TranslationError("Path does not exist", path)
+        if not path.is_dir():
+            raise TranslationError("Path is not a directory", path)
+        self.path = path
+        return None
 
-    Parameters:
-    - language: The language code to set as the preferred language
+    def get_language(self) -> str:
+        return self.language
 
-    Returns: None
-    '''
-    # Check if the language is available
-    if language == "invalid":
-        set_config("language", language)
-    elif language not in get_translations():
-        raise TranslationError("Language not found", language)
-    else:
-        set_config("language", language)
-    return None
+    def get_fallback_language(self) -> str:
+        return self.fallback_language
+
+    def get_path(self) -> Path:
+        return self.path
+
+############################################################
+#                         # FILE IO                        #
+############################################################
+    def load_translations(self) -> None:
+        self.translations = {}
+        ## YAML Support
+        for file in self.path.glob('*.yaml'):
+            lang_code = file.stem  # Extract the language code from the filename
+            with file.open('r') as f:
+                self.translations[lang_code] = yaml.safe_load(f)
+        for file in self.path.glob('*.yml'):
+            lang_code = file.stem  # Extract the language code from the filename
+            with file.open('r') as f:
+                self.translations[lang_code] = yaml.safe_load(f)
+        ## JSON Support
+        for file in self.path.glob('*.json'):
+            lang_code = file.stem  # Extract the language code from the filename
+            with file.open('r') as f:
+                self.translations[lang_code] = json.load(f)
+        return None
+
+
+############################################################
+#                       # TRANSLATION                      #
+############################################################
+
+    def translate(self, translation_key: str, **kwargs) -> str:
+        # Helper function to get translation from a specific language
+        def get_translation(language, keys):
+            translation = self.translations.get(language, {})
+            for key in keys:
+                if isinstance(translation, dict):
+                    translation = translation.get(key)
+                else:
+                    return None
+            return translation
+        keys = translation_key.split('.')
+        translation = get_translation(self.language, keys) or get_translation(self.fallback_language, keys)
+        if translation:
+            return translation.format(**kwargs)
+        else:
+            raise TranslationError(
+                f"Translation key '{translation_key}' not found in selected language '{self.language}' or fallback language '{self.fallback_language}'", translation_key)
+
+    def pluralize(self, translation_key: str, count: int) -> str:
+        # Helper function to get translation from a specific language
+        def get_translation(language, keys):
+            translation = self.translations.get(language, {})
+            for key in keys:
+                if isinstance(translation, dict):
+                    translation = translation.get(key)
+                else:
+                    return None
+            return translation
+        keys = translation_key.split('.')
+        translation = get_translation(self.language, keys) or get_translation(self.fallback_language, keys)
+        if translation:
+            if count == 1:
+                return translation.get('one')
+            elif count > 1:
+                return translation.get('many')
+            elif count == 0:
+                return translation.get('none')
+            else:
+                return translation.get('default')
+        else:
+            raise TranslationError(
+                f"Translation key '{translation_key}' not found in selected language '{self.language}' or fallback language '{self.fallback_language}'", translation_key)
