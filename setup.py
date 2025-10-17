50916c332dbcc5af3cfe2f68edb95aba84945918
CEOXeon <127042438+CEOXeon@users.noreply.github.com>
2025-10-17 19:38:38 +0200

    Code Cleanup

--- setup.py
+++ setup.py
@@ -3,26 +3,6 @@ from pathlib import Path
 import setuptools
 import subprocess
 
-#VERSION = "0.0.2"  # PEP-440
-#NAME = "translatium"
-#DESCRIPTION = "A pure Python i18n library for your Python projects."
-#URL="https://github.com/CEOXeon/Translatium"
-#PROJECT_URLS = {
-#    "Source Code": "https://github.com/CEOXeon/Translatium",
-#}
-#AUTHOR = "Louis Zimmermann (@CEOXeon)"
-#AUTHOR_EMAIL = "louis-github@tutanota.com"
-#LICENSE = "Apache-2.0"
-#CLASSIFIERS = [
-#    "Development Status :: 2 - Pre-Alpha",
-#    "Intended Audience :: Developers",
-#    "Programming Language :: Python :: 3",
-#    "Programming Language :: Python :: 3.12",
-#]
-#INSTALL_REQUIRES = [
-#    "pyyaml",
-#]
-
 class CleanCommand(setuptools.Command):
     description = "Clean up the files using .gitignore"
     user_options = []
@@ -77,18 +57,6 @@ class InstallProdCommand(setuptools.Command):
 
 
 setuptools.setup(
-    #name=NAME,
-    #version=VERSION,
-    #description=DESCRIPTION,
-    #url=URL,
-    #project_urls=PROJECT_URLS,
-    #author=AUTHOR,
-    #author_email=AUTHOR_EMAIL,
-    #license=LICENSE,
-    #classifiers=CLASSIFIERS,
-    ##python_requires=">=3.8",
-    ## Requirements
-    #install_requires=INSTALL_REQUIRES,
     packages=setuptools.find_packages(where="src"),
     package_dir={"": "src"},
     long_description=Path("README.md").read_text(),
